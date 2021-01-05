be3ead2e2fed054b2f699c72e0035bc6cc9f63bb
DB <YeQingDinBin@yeah.net>
2021-01-05 22:42:10 +0800

    first

---
+++ first/src/first/Hello.java
@@ -0,0 +1,16 @@
+package first;
+
+public class Hello 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		System.out.println("我今年"+19+"岁！");
+		System.out.println("我的职业是学生");
+		System.out.println("我想学好java!");
+		System.out.println("我高考数学："+123+"分;"+"英语："
+		+102+"分"+"语文："+110+"分"+"文综："+204+"分");
+	}
+}
---
+++ first/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ding'Bing
+ *
+ */
+module first {
+}
