dd497c4d36bb7cf55b5824553b682d9e950ac155
DB <YeQingDinBin@yeah.net>
2021-01-06 19:14:18 +0800

    java

---
+++ test_1.6/src/java.java
@@ -0,0 +1,22 @@
+
+public class java
+{
+
+	public static void main(String[] args)
+	{
+		//小括号优先一切
+		// TODO Auto-generated method stub
+		String str1="Hellow";
+		System.out.println(str1);
+		System.out.println(str1+20+21);
+		System.out.println(str1+(20+30));
+		int i=0;
+		int sum=0;
+		for(i=1;i<10;i++)
+		{
+			sum=sum+1;
+		}
+		System.out.println("sum = "+sum);
+	}
+	
+}
