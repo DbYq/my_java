bae3dc3a0113a1ee839932b77e646e2260f7520e
DB <YeQingDinBin@yeah.net>
2021-01-08 20:56:31 +0800

    java

---
+++ test_21.1.8/src/test.java
@@ -0,0 +1,39 @@
+
+//public class test
+//{
+//
+//	public static void main(String[] args)
+//	{
+//		// TODO Auto-generated method stub
+//		int i;
+//		for(i=100;i<=999;i++)
+//		{
+//			int z,k,h;
+//			z=i/100%10;
+//			k=i/10%10;
+//			h=i%10;
+//			if(Math.pow(z, 3)+Math.pow(k, 3)+Math.pow(h, 3)==i)
+//			{
+//				System.out.println(i);
+//			}
+//		}
+//	}
+//
+//}
+//public class test
+//{
+//
+//	public static void main(String[] args)
+//	{
+//		// TODO Auto-generated method stub
+//		int i,j;
+//		for(i=1;i<=9;i++)
+//		{
+//			for(j=1;j<=i;j++)
+//			{
+//				System.out.println(i+"*"+j+"="+i*j+"\t");
+//			}
+//			System.out.println();
+//		}
+//	}
+//}
