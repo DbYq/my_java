4b232c61d6026eaa408540a8fb32bb5c40f52f0e
DB <YeQingDinBin@yeah.net>
2021-01-21 19:06:08 +0800

    数组

---
+++ arrysLearn/src/arrysLearn/test.java
@@ -0,0 +1,28 @@
+package arrysLearn;
+
+import java.util.Arrays;
+
+/*
+ * public class test {
+ * 
+ * public static void main(String[] args) { // TODO Auto-generated method stub
+ * int[] arr=new int[10]; for(int i=0;i<10;i++) {
+ * arr[i]=(int)(Math.random()*(99-10+1)+10); for(int j=0;j<i;j++) {
+ * if(arr[i]==arr[j]) { arr[i]=(int)(Math.random()*(99-10+1)+10); } } }
+ * System.out.println(Arrays.toString(arr)); }
+ * 
+ * }
+ */
+/*
+ * public class test { public static void main(String[] args) { int[]
+ * arr={5,7,8,3,9,0,4,6,1,2}; int i,j; for(i=0;i<=arr.length-1;i++) {
+ * for(j=0;j<arr.length-1-i;j++) { if(arr[j]>arr[j+1]) { int temp; temp=arr[j];
+ * arr[j]=arr[j+1]; arr[j+1]=temp; } } }
+ * System.out.println(Arrays.toString(arr)); for(i=0;i<arr.length/2;i++) { int
+ * temp=arr[i]; arr[i]=arr[arr.length-1-i]; arr[arr.length-1-i]=temp; }
+ * System.out.println(Arrays.toString(arr)); int[] arr2=arr;
+ * System.out.println(Arrays.toString(arr2)); int z=1;
+ * for(i=0;i<arr2.length;i++) { if(arr2[i]==22) { System.out.println(i); z=0; }
+ * } if(z==1) { System.out.println("找不到"); } } }
+ */
+ 
