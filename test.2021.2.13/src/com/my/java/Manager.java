d0e7daae8514399e583b5a5bb52a08586e86935f
DB <YeQingDinBin@yeah.net>
2021-02-13 23:49:31 +0800

    ，

---
+++ test.2021.2.13/src/com/my/java/Manager.java
@@ -0,0 +1,14 @@
+package com.my.java;
+
+public class Manager extends Employee
+{
+
+	private double bonus;
+	public Manager(String name, double salary, int year, int month, int day)
+	{
+		super(name, salary, year, month, day);
+		// TODO Auto-generated constructor stub
+	}
+	public 
+	
+}
